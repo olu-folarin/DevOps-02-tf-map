5dd7fc7f7dc95f8f66f4cbbcdaa11883696d6708
olufolarin <folami25@gmail.com>
2022-02-11 12:08:34 +0100

    set up a virtual server with AWS's EC2 instance

---
+++ output.tf
@@ -0,0 +1,3 @@
+output "aws_security_group_http_server_info" {
+  value = aws_security_group.http_server_sg
+}
---
+++ resources.tf
@@ -0,0 +1,46 @@
+// first create an HTTP server: 80 TCP, 22 TCP, CIDR ["0.0.0.0/0"]
+// then create a security group with the above config
+
+
+resource "aws_security_group" "http_server_sg" {
+  name   = "http_server_sg"
+  vpc_id = "vpc-075343a660b9eb15c"
+
+  // IN -> ingress: where to allow traffic from
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  // IN -> ingress for ssh
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  // OUT -> egress: what can you do from this server
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    name = "http_server_sg"
+  }
+}
+
+// create a virtual server
+resource "aws_instance" "http_server" {
+    ami = "ami-033b95fb8079dc481"
+    key_name = 
+    instance_type = "t2_micro"
+    vpc_security_group_ids = [aws_security_group.http_server_sg.id]
+    // get this from vpc on aws
+    subnet_id = "subnet-03f0d4627c28d93a5"
+}
