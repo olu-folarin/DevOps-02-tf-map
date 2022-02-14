c4dc1d40da79d12203171d5f4223a9c860dd7db7
olufolarin <folami25@gmail.com>
2022-02-14 11:59:51 +0100

    provided the name, owner(current provider) and filter

--- provider.tf
+++ provider.tf
@@ -5,5 +5,10 @@ data "aws_subnet_ids" "default_subnets" {
 
 // ami data provider
 data "aws_ami" "latest_aws_linux_2" {
-    
+    most_recent = true
+    owners = ["amazon"]
+    filter {
+        name = "name"
+        values = ["amzn2-ami-hvm-*"]
+    }
 }
